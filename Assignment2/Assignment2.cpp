18724012a4ba76bead9c93fe65619ae9b06cb7dc
Miguel Angelo Bautista <105768284+MiguelAngeloBautista@users.noreply.github.com>
2024-05-24 21:05:00 +0930

    Add project files.

---
+++ Assignment2/Assignment2.cpp
@@ -0,0 +1,12 @@
+﻿// Assignment2.cpp : Defines the entry point for the application.
+//
+
+#include "Assignment2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
